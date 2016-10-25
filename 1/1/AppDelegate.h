1e0ae152c3cc1e8bce5b8e0b3bcb3d170b691639
hensonChang <henson_chang@yeah.net>
2016-10-25 12:03:44 +0800

    第一次提交

---
+++ 1/1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1
+//
+//  Created by 张海生 on 16/10/25.
+//  Copyright © 2016年 张海生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 1/1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1
+//
+//  Created by 张海生 on 16/10/25.
+//  Copyright © 2016年 张海生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
